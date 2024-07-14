7eb84216654f71536c59c4b73025e5d688ef8a72
yash-op7 <yashm3375@gmail.com>
2024-07-14 12:26:41 +0530

    completed the server side data fetch example

--- src/app/fetch-example/page.tsx
+++ src/app/fetch-example/page.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+interface User {
+    id:number,
+    name:string
+}
 
 const DataFetchExample = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users = await res.json();
-    console.log(users);
+    const users: User[] = await res.json();
+    console.log(users); // nothing can be seen as this is being done on the server 
   return (
     <div>
-      whoa
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
     </div>
   )
 }
